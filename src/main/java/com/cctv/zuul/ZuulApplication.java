320f607c73fe14272b752cf3ab5e81002ef9b33b
easy89757 <easy89757@126.com>
2019-12-13 17:37:54 +0800

    添加各个服务框架代码。

---
+++ src/main/java/com/cctv/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.cctv.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author zb
+ * @create 2019-12-12 15:21
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+}
